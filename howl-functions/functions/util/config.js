8750a04666163a7a604178aa417ce73d244dc0f7
Santiago Franco <santiagofrancogarzon@gmail.com>
2020-09-07 22:17:41 -0400

    Update config.js

--- howl-functions/functions/util/config.js
+++ howl-functions/functions/util/config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  apiKey: "AIzaSyCDb1LdMa6uDrWN80zzggzYzpP_uVPOl2U",
-  authDomain: "howl-62fe0.firebaseapp.com",
-  databaseURL: "https://howl-62fe0.firebaseio.com",
-  projectId: "howl-62fe0",
-  storageBucket: "howl-62fe0.appspot.com",
-  messagingSenderId: "954034255208",
-  appId: "1:954034255208:web:dd1fa0b9bba452c3ab92dd",
-  measurementId: "G-0ZC29G5VWV",
+  apiKey: *,
+  authDomain: *,
+  databaseURL: *,
+  projectId: *,
+  storageBucket: *,
+  messagingSenderId: *,
+  appId:*,
+  measurementId: *,
 };
